d04fe1f586b07c687a132d7d373ffa97fff9f0d4
valladao <valladao@gmail.com>
2021-06-07 20:43:16 -0300

    path module test

--- hello.js
+++ hello.js
@@ -1,2 +1,11 @@
-const datefns = require("date-fns")
-console.log(datefns.format(new Date(2021, 1, 11), "yyyy-MM-dd"))
+const path = require("path")
+
+//const name = "flavio"
+
+const a = path.resolve("flavio.txt")
+const b = path.resolve("tmp", "flavio.txt")
+const c = path.resolve("/etc", "flavio.txt")
+
+console.log(a)
+console.log(b)
+console.log(c)
